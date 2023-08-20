a6f44b3f5116b82be8b66b207c0aed2c4329f2e6
laimuf <youremail@yourdomain.com>
2023-08-20 16:15:00 +0100

    Improve cost estimate

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ LIMIT = 100  # None for all posts
 
 # OpenAI params
 model = "gpt-3.5-turbo"
+input_price_1k_tokens = 0.0015
+output_price_1k_tokens = 0.002
+
 n_posts = 10
 file_name = "data/exmuslim_hot_Noneposts_2023_06_18_18_25_48.xlsx"
 file_name_gpt = f"data/exmuslim_hot_Noneposts_2023_06_18_18_25_48_gpt_{n_posts}.xlsx"
