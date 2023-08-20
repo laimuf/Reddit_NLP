a132a7f9481b95472f7496371a6ae94319056674
laimuf <113716215+laimuf@users.noreply.github.com>
2023-08-20 15:40:41 +0100

    Add cost estimate (#12)

--- add_gpt_info.py
+++ add_gpt_info.py
@@ -1,3 +1,4 @@
+import tiktoken
 import time 
 import json
 import openai
@@ -20,11 +21,35 @@ import pandas as  pd
 openai.api_key = openai_key
 
 
+def cost_estimate(df, model, input_price_1k_tokens=0.0015, output_price_1k_tokens=0.002):
+
+    n_queries = df.shape[0]
+
+    expected_num_response_tokens = 100
+    responses_cost = output_price_1k_tokens * (expected_num_response_tokens / 1_000) * n_queries
+
+    full_text = " ".join(df["title"].tolist()) + " ".join([ txt for txt in df["selftext"].tolist() if isinstance(txt, str)])
+
+    # To get the tokeniser corresponding to a specific model in the OpenAI API:
+    enc = tiktoken.encoding_for_model(model)
+    
+    n_input_tokens = len(enc.encode(full_text))
+
+    query_cost = input_price_1k_tokens * (n_input_tokens / 1_000)
+
+    resp = input(f"Estimated cost ${query_cost + responses_cost :0.4f}. Enter `yes` to continue:\n")
+
+    assert resp == "yes", "Do not proceed with GPT queries"
+
+
+
 if __name__ == "__main__":
 
     df = pd.read_excel(file_name)
 
     df = df.iloc[1:n_posts+1]
+
+    cost_estimate(df, model)
 
     responses_topics = {}
     for topic in topics_list:
