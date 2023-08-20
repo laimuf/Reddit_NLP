43ff34db9b87176eecbfd9a7bbb187e257b57b9e
laimuf <113716215+laimuf@users.noreply.github.com>
2023-08-20 16:16:22 +0100

    Improve cost estimate (#13)

--- add_gpt_info.py
+++ add_gpt_info.py
@@ -12,6 +12,8 @@ from config import (
     n_posts,
     file_name,
     file_name_gpt,
+    input_price_1k_tokens,
+    output_price_1k_tokens,
 )
 
 from tqdm import tqdm
@@ -21,21 +23,26 @@ import pandas as  pd
 openai.api_key = openai_key
 
 
-def cost_estimate(df, model, input_price_1k_tokens=0.0015, output_price_1k_tokens=0.002):
+def cost_estimate(df, model, system_prompt, question, 
+    input_price_1k_tokens=input_price_1k_tokens, 
+    output_price_1k_tokens=output_price_1k_tokens):
 
     n_queries = df.shape[0]
 
     expected_num_response_tokens = 100
     responses_cost = output_price_1k_tokens * (expected_num_response_tokens / 1_000) * n_queries
 
-    full_text = " ".join(df["title"].tolist()) + " ".join([ txt for txt in df["selftext"].tolist() if isinstance(txt, str)])
+    full_text = (" ".join(df["title"].tolist()) 
+                + " ".join([ txt for txt in df["selftext"].tolist() if isinstance(txt, str)])
+                + (system_prompt + question) * n_queries
+    )
 
     # To get the tokeniser corresponding to a specific model in the OpenAI API:
     enc = tiktoken.encoding_for_model(model)
     
     n_input_tokens = len(enc.encode(full_text))
 
-    query_cost = input_price_1k_tokens * (n_input_tokens / 1_000)
+    query_cost = input_price_1k_tokens * (n_input_tokens / 1_000) 
 
     resp = input(f"Estimated cost ${query_cost + responses_cost :0.4f}. Enter `yes` to continue:\n")
 
@@ -49,12 +56,15 @@ if __name__ == "__main__":
 
     df = df.iloc[1:n_posts+1]
 
-    cost_estimate(df, model)
+    cost_estimate(df, model, system_prompt, question)
 
     responses_topics = {}
     for topic in topics_list:
         responses_topics[topic] = []
     responses_comments = []
+
+    n_input_tokens = 0
+    n_output_tokens = 0
 
     for id, row in tqdm(df.iterrows()):
         time.sleep(1)
@@ -71,10 +81,14 @@ if __name__ == "__main__":
                 {"role": "user", "content": user_question},
             ]
         )
+
+        n_input_tokens += response["usage"]["prompt_tokens"]
+        n_output_tokens += response["usage"]["completion_tokens"]
         gpt_response = response['choices'][0]['message']['content']
         gpt_response_dict = json.loads(gpt_response)
 
         # Add try-except to filter bad responses
+        df_checkpoint = df.iloc[: id].copy()
         
         for topic in topics_list:
             topic_found = False
@@ -83,11 +97,13 @@ if __name__ == "__main__":
             responses_topics[topic].append(topic_found)
             df_checkpoint[topic] = responses_topics[topic]
 
-        df_checkpoint = df.iloc[: id].copy()
         responses_comments.append(gpt_response_dict["comment"])
         df_checkpoint["gpt_comment"] = responses_comments
 
         # Save progress
         df_checkpoint.to_excel(file_name_gpt)
+    tot_cost = output_price_1k_tokens * (n_output_tokens/1_000) + input_price_1k_tokens * (n_input_tokens/1_000)
+    print(f"Total cost: ${tot_cost :.5f}")
+    print(f"Data saved at {file_name_gpt}")
 
 
